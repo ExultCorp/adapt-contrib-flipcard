a4709c5485dd123c4008a3570ed0478e20cfcf55
Himanshu Rajotia <hrajotia@gmail.com>
2014-05-30 14:20:05 +0530

    version 0.0.2
    According to Adapt Framework version 1.1.0

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
-adapt-contrib-flipCard
-======================
+#adapt-contrib-flipCard
 
-Flip Card Component/Plug-in developed by CrediPoint Solutions for Adapt Framework v1.0.0.
+Flip Card Component/Plug-in developed by CrediPoint Solutions for Adapt Framework v1.1.0.
 
 A basic flip card Component/Plug-in that generates cards with an image on the front face and text on the back face.
 
-Installation
-------------
+##Installation
 
 First, be sure to install the [Adapt Command Line Interface](https://github.com/cajones/adapt-cli), then from the command line run:
 
 		adapt install adapt-contrib-flipCard
 
-Usage
-------
+##Usage
 
 For example JSON format, see [example.json](https://github.com/CrediPointSolutions/adapt-contrib-flipCard/blob/master/example.json).
 
@@ -29,8 +26,10 @@ Individual Flip Example: All flip cards can flip at any given point of time and 
 		"_classes":"",
 		"_layout":"full",
 		"title": "Flip Card",
+		"displayTitle": "Flip Card",
 		"body": "This is optional body text. Click the image below to reveal the text.",
-		"items": [
+		"instruction": "",
+		"_items": [
 			{
 				"frontImageSrc":"assets/flip_1.png",
 				"backHeading":"To overcome Global Climate Change challenges, it is essential to:",
@@ -64,8 +63,10 @@ Single Flip Example: Only one flip card can flip at any given point of time and 
 	 	"_classes":"",
 	 	"_layout":"full",
 	 	"title": "Flip Card",
+	 	"displayTitle": "Flip Card",
 	 	"body": "This is optional body text. Click the image below to reveal the text.",
-	 	"items": [
+	 	"instruction": "",
+	 	"_items": [
 	 		{
 	 			"frontImageSrc":"assets/flip_1.png",
 	 			"backHeading":"To overcome Global Climate Change challenges, it is essential to:",
@@ -88,9 +89,65 @@ Single Flip Example: Only one flip card can flip at any given point of time and 
 	 	"_flipType": "singleFlip",
 	 	"_flipTime":400
 	}
+##Settings overview
 
-Important
----------
+For example JSON format, see [example.json](https://github.com/CrediPointSolutions/adapt-contrib-flipCard/blob/master/example.json)
+
+The following explains further settings for the flipCard component:
+
+####_component
+
+This value must be: `flipCard`
+
+####_classes
+
+You can use this setting to add custom classes to your template and LESS file.
+
+####_layout
+
+This defines the position of the component in the block. Values can be `full`, `left` or `right`.
+
+####_items
+
+Each item represents one element of the flipCard. Text values can be entered for `frontImageSrc`, and `backHeading` for each element.
+
+####alt
+
+The alt setting provides alternative information for the image.
+
+####title
+
+Enter a title value for this flipCard element. This text is displayed at all times.
+
+####body
+
+The text entered for the body will be shown when the reader open this flipCard element.
+
+####audioSrc
+
+The path of the audio file without extension for playing when the reader opens this flipCard element.
+
+####audioTypes
+
+The types of audio codec supported by this flipCard element.
+
+####type
+
+The audio file extension which is mentioned in audioSrc for this flipCard element.
+
+####codec
+
+The html5 equivalent codec name for current audio type.
+
+##Limitations
+
+To be completed.
+
+##Browser spec
+
+This component has been tested to the standard Adapt browser specification.
+
+##Important
 
 This component works in IE9- but has slight change in behaviour, instead of flip effect it gets a fade-in/out effect.
 
--- js/adapt-contrib-flipCard.js
+++ js/adapt-contrib-flipCard.js
@@ -4,37 +4,37 @@
  * Maintainers - Himanshu Rajotia <himanshu.rajotia@credipoint.com>, CrediPoint Solutions <git@credipoint.com>
  */
 define(function(require) {
-  var ComponentView = require('coreViews/componentView');
-  var Adapt = require('coreJS/adapt');
+  var ComponentView = require("coreViews/componentView");
+  var Adapt = require("coreJS/adapt");
 
   var FlipCard = ComponentView.extend({
 
     events: {
-      'click .flipCard-widget .flipCard-item': 'flipItem'
+      "click .flipCard-widget .flipCard-item": "flipItem"
     },
 
     preRender: function() {
-      this.listenTo(Adapt, 'device:resize', this.reRender, this);
-      this.listenTo(Adapt, 'device:changed', this.reRender, this);
+      this.listenTo(Adapt, "device:resize", this.reRender, this);
+      this.listenTo(Adapt, "device:changed", this.reRender, this);
     },
 
     // this is use to set ready status for current component on postRender.
     postRender: function() {
       ComponentView.prototype.postRender.apply(this);
       var self = this;
-      this.$('.flipCard-item-frontImage:first').load(function() {
+      this.$(".flipCard-item-frontImage:first").load(function() {
         self.reRender();
       });
       if (!Modernizr.csstransforms3d) {
-        this.$('.flipCard-item-back').hide();
+        this.$(".flipCard-item-back").hide();
       }
       this.setReadyStatus();
     },
 
     // This function called on triggering of device resize and device change event of Adapt.
     reRender: function() {
-      var $flipCardContainer = this.$('.flipCard-item');
-      var imageHeight = this.$('.flipCard-item-frontImage:first').height();
+      var $flipCardContainer = this.$(".flipCard-item");
+      var imageHeight = this.$(".flipCard-item-frontImage:first").height();
       if (imageHeight) {
         $flipCardContainer.height(imageHeight);
       }
@@ -42,12 +42,12 @@ define(function(require) {
 
     // Click or Touch event handler for flip card.
     flipItem: function(event) {
-      if (event) event.preventDefault();
+      event.preventDefault();
       var $selectedElement = $(event.target);
-      var flipType = this.model.get('_flipType');
-      if (flipType === 'individualFlip') {
+      var flipType = this.model.get("_flipType");
+      if (flipType === "individualFlip") {
         this.performIndividualFlip($selectedElement);
-      } else if (flipType === 'singleFlip') {
+      } else if (flipType === "singleFlip") {
         this.performSingleFlip($selectedElement);
       }
     },
@@ -56,30 +56,30 @@ define(function(require) {
     // where all cards can flip and stay in the flipped state.
     performIndividualFlip: function($selectedElement) {
       var $flipCardItem;
-      if ($selectedElement.hasClass('flipCard-item')) {
+      if ($selectedElement.hasClass("flipCard-item")) {
         $flipCardItem = $selectedElement;
       } else {
-        $flipCardItem = $selectedElement.closest('.flipCard-item');
+        $flipCardItem = $selectedElement.closest(".flipCard-item");
       }
 
       if (!Modernizr.csstransforms3d) {
-        var $frontFlipCard = $flipCardItem.find('.flipCard-item-front');
-        var $backFlipCard = $flipCardItem.find('.flipCard-item-back');
-        var flipTime = this.model.get('_flipTime') | 'fast';
-        if ($frontFlipCard.is(':visible')) {
+        var $frontFlipCard = $flipCardItem.find(".flipCard-item-front");
+        var $backFlipCard = $flipCardItem.find(".flipCard-item-back");
+        var flipTime = this.model.get("_flipTime") | "fast";
+        if ($frontFlipCard.is(":visible")) {
           $frontFlipCard.fadeOut(flipTime, function() {
             $backFlipCard.fadeIn(flipTime);
           });
-        } else if ($backFlipCard.is(':visible')) {
+        } else if ($backFlipCard.is(":visible")) {
           $backFlipCard.fadeOut(flipTime, function() {
             $frontFlipCard.fadeIn(flipTime);
           });
         }
       } else {
-        $flipCardItem.toggleClass('flipCard-flip');
+        $flipCardItem.toggleClass("flipCard-flip");
       }
 
-      var flipCardElementIndex = this.$('.flipCard-item').index($flipCardItem);
+      var flipCardElementIndex = this.$(".flipCard-item").index($flipCardItem);
       this.setVisited(flipCardElementIndex);
     },
 
@@ -87,27 +87,27 @@ define(function(require) {
     // only one card can flip and stay in the flipped state.
     performSingleFlip: function($selectedElement) {
       var $flipCardItem;
-      if ($selectedElement.hasClass('flipCard-item')) {
+      if ($selectedElement.hasClass("flipCard-item")) {
         $flipCardItem = $selectedElement;
       } else {
-        $flipCardItem = $selectedElement.closest('.flipCard-item');
+        $flipCardItem = $selectedElement.closest(".flipCard-item");
       }
 
-      var flipCardContainer = $flipCardItem.closest('.flipCard-widget');
+      var flipCardContainer = $flipCardItem.closest(".flipCard-widget");
       if (!Modernizr.csstransforms3d) {
-        var frontFlipCard = $flipCardItem.find('.flipCard-item-front');
-        var backFlipCard = $flipCardItem.find('.flipCard-item-back');
-        var flipTime = this.model.get('_flipTime') | 'fast';
+        var frontFlipCard = $flipCardItem.find(".flipCard-item-front");
+        var backFlipCard = $flipCardItem.find(".flipCard-item-back");
+        var flipTime = this.model.get("_flipTime") | "fast";
 
-        if (backFlipCard.is(':visible')) {
+        if (backFlipCard.is(":visible")) {
           backFlipCard.fadeOut(flipTime, function() {
             frontFlipCard.fadeIn(flipTime);
           });
         } else {
-          var visibleFlipCardBack = flipCardContainer.find('.flipCard-item-back:visible');
+          var visibleFlipCardBack = flipCardContainer.find(".flipCard-item-back:visible");
           if (visibleFlipCardBack.length > 0) {
             visibleFlipCardBack.fadeOut(flipTime, function() {
-              flipCardContainer.find('.flipCard-item-front:hidden').fadeIn(flipTime);
+              flipCardContainer.find(".flipCard-item-front:hidden").fadeIn(flipTime);
             });
           }
           frontFlipCard.fadeOut(flipTime, function() {
@@ -115,43 +115,44 @@ define(function(require) {
           });
         }
       } else {
-        if ($flipCardItem.hasClass('flipCard-flip')) {
-          $flipCardItem.removeClass('flipCard-flip');
+        if ($flipCardItem.hasClass("flipCard-flip")) {
+          $flipCardItem.removeClass("flipCard-flip");
         } else {
-          flipCardContainer.find('.flipCard-item').removeClass('flipCard-flip');
-          $flipCardItem.addClass('flipCard-flip');
+          flipCardContainer.find(".flipCard-item").removeClass("flipCard-flip");
+          $flipCardItem.addClass("flipCard-flip");
         }
       }
 
-      var flipCardElementIndex = this.$('.flipCard-item').index($flipCardItem);
+      var flipCardElementIndex = this.$(".flipCard-item").index($flipCardItem);
       this.setVisited(flipCardElementIndex);
     },
 
     // This function will set the visited status for particular flipCard item.
     setVisited: function(index) {
-      var item = this.model.get('items')[index];
+      var item = this.model.get("_items")[index];
       item._isVisited = true;
       this.checkCompletionStatus();
     },
 
     // This function will be used to get visited states of all flipCard items.
     getVisitedItems: function() {
-      return _.filter(this.model.get('items'), function(item) {
+      return _.filter(this.model.get("_items"), function(item) {
         return item._isVisited;
       });
     },
 
     // This function will check or set the completion status of current component.
     checkCompletionStatus: function() {
-      if (!this.model.get('_isComplete')) {
-        if (this.getVisitedItems().length === this.model.get('items').length) {
+      if (!this.model.get("_isComplete")) {
+        if (this.getVisitedItems().length === this.model.get("_items").length) {
           this.setCompletionStatus();
         }
       }
     }
   });
 
-  Adapt.register('flipCard', FlipCard);
+  Adapt.register("flipCard", FlipCard);
+
   return FlipCard;
 
 });
